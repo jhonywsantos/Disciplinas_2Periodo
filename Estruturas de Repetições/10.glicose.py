bc0e77aa47e762699783aa4f10f95a8e07482182
Jhony W. Santos <154033677+jhonywsantos@users.noreply.github.com>
2023-12-18 15:41:37 -0300

    Update 10.glicose.py

--- Estruturas de Repetições/10.glicose.py
+++ Estruturas de Repetições/10.glicose.py
@@ -1,7 +1,7 @@
-'''Escreva um programa que receba vários valores da taxa de glicose de Genival, até que o valor
-informado seja 0, e calcule a glicose média observada naquele dia.Caso esse valor seja inferior a 110, 
-o programa deve exibir a mensagem "Glicose Normal". Caso seja 200 ou mais, a mensagem exibida deve ser 
-“Glicose Muito Alta". Nos demais casos, o programa deve exibir “Glicose Alterada”.'''
+'''Escreva um programa que receba vários valores da taxa de glicose de Genival, até que o valor informado seja 0, 
+e calcule a glicose média observada naquele dia.Caso esse valor seja inferior a 110, o programa deve exibir a men-
+sagem "Glicose Normal". Caso seja 200 ou mais, a mensagem exibida deve ser “Glicose Muito Alta". Nos demais casos, 
+o programa deve exibir “Glicose Alterada”.'''
 
 soma_glicose = 0
 contador_valores = 0
